988e46dfed13a4659abab6623206601e518421ee
Sean Granier <seanlg624@gmail.com>
2020-10-12 16:18:05 -0500

    practice is good

---
+++ codecademy_practice.py
@@ -0,0 +1,25 @@
+def greet_customer(grocery_store, special_item):
+    print("Welcome to " + grocery_store + ".")
+    print("Our special is " + special_item + ".")
+    print("Have fun shopping!")
+
+
+greet_customer("Stu's Staples", "papayas")
+
+
+##
+
+
+def mult_two_add_three(number):
+    print(number * 2 + 3)
+
+
+# Call mult_two_add_three() here:
+mult_two_add_three(15)
+
+
+def mult_x_add_y(number, x, y):
+    print(number * x + y)
+
+
+mult_x_add_y(1, 3, 1)
